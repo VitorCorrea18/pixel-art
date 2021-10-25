0c3239772732e7ec78695f491badf951a7c004ae
Vitor Correa <vitorcorrea18@gmail.com>
2021-10-25 17:48:20 -0400

    implemntando os eventos

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
-const btnBlack = document.getElementById('black');
-const btnBlue = document.getElementById('blue');
-const btnGreen = document.getElementById('green');
-const btnRed = document.getElementById('red');
+window.onload = function () {
+  const btnBlack = document.getElementById('black');
+  const btnBlue = document.getElementById('blue');
+  const btnGreen = document.getElementById('green');
+  const btnRed = document.getElementById('red');
 
-window.onload = function(){
+  btnBlack.classList.add('selected');
+  
+  btnBlue.addEventListener('click', function(event) {
+    let selectedElemt = document.querySelector('.selected');
+    selectedElemt.classList.remove('selected');
+    event.target.classList.add('selected');
+  })
+  
+  btnGreen.addEventListener('click', function(event) {
+    let selectedElemt = document.querySelector('.selected');
+    selectedElemt.classList.remove('selected');
+    event.target.classList.add('selected');
+  })
 
-btnBlack.classList.add('selected');
+  btnRed.addEventListener('click', function(event) {
+    let selectedElemt = document.querySelector('.selected');
+    selectedElemt.classList.remove('selected');
+    event.target.classList.add('selected');
+  })
+
+  btnBlack.addEventListener('click', function(event) {
+    let selectedElemt = document.querySelector('.selected');
+    selectedElemt.classList.remove('selected');
+    event.target.classList.add('selected');
+  })
+
+  const btnPixel = document.querySelector('.pixel');
+
+  btnPixel.addEventListener('click', function(event) {
+    let selectedElemt = document.querySelector('.selected');
+    console.log('clikado');
+  })
+
+
 
 }
-
-
