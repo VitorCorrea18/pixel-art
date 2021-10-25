21bfbf7b584c980d9e7a11374f6c9d72a01986de
Vitor Correa <vitorcorrea18@gmail.com>
2021-10-25 17:02:06 -0400

    iniciando o JS

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function(){
+const btnBlack = document.getElementById('black');
+const btnBlue = document.getElementById('blue');
+const btnGreen = document.getElementById('green');
+const btnRed = document.getElementById('red');
+
+btnBlack.classList.add('selected');
+
+
+
+
+}
